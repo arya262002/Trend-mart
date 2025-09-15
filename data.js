22ea323d91b730b1471d9f97b8a798112ddfa9db
Arpit Rastogi <104339792+arya262002@users.noreply.github.com>
2025-09-15 10:26:59 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.seedhemaut.com/wp-content/uploads/2023/08/01-1.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "e2e1e4",
+    },
+    {
+      id: 2,
+      img: "https://www.myfandom.store/media/catalog/product/cache/d29dc733f19a0229b9c7dddb18d84d38/o/f/official_shah_rule_merchandise_your_majesty_oversize_black_tshirt_back.jpg",
+      title: "T-SHIRT COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f3f5f4",
+    },
+    {
+      id: 3,
+      img: "https://assets.ajio.com/medias/sys_master/root/20231116/zzfC/65560733ddf779151989345f/-473Wx593H-442271702-grey-MODEL5.jpg",
+      title: "WINTER COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "eeeef0",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://wrogn.com/cdn/shop/files/4_3a126de6-ac8c-457c-9f9f-c2313b5edd48.jpg?v=1713803372&width=375",
+      title: "T SHIRTS",
+      cat: "tshirt"
+    },
+    {
+      id: 2,
+      img: "https://wrogn.com/cdn/shop/files/3_52979390-061c-4972-9a6d-d576676db2b4.jpg?v=1707829306&width=550",
+      title: "JEANS",
+      cat: "jeans"
+    },
+    {
+      id: 3,
+      img: "https://grazia.wwmindia.com/content/2021/apr/krsna1617891000.jpg",
+      title: "LIGHT JACKETS",
+      cat: "jacket"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://www.seedhemaut.com/wp-content/uploads/2022/07/clouds-1.jpg",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://gullygangmerch.com/cdn/shop/files/GGLOGO_FRONT_360x.png?v=1698227918",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://www.urbanmonkey.com/cdn/shop/products/radioactive-left-view_1024x.jpg?v=1656638783",
+    },
+    {
+      id:7,
+      img:"https://gullygangmerch.com/cdn/shop/products/RedBandana3_360x.png?v=1663663695",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
